1d03bb85d8cd147bbd62d91740602c3e6ccdfe81
Vineeth Duvvuri <vineethdvs123@gmail.com>
2022-01-31 12:23:49 +0530

    Student model set up without images, seeds working

--- models/student.js
+++ models/student.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-
+const ImageSchema = new Schema({
+	url: String,
+	filename: String,
+});
 const StudentSchema = new Schema({
 	name: String,
 	roll: String,
 	regNo: String,
 	department: String,
+	// photo: ImageSchema,
+	// gradeCards: [ImageSchema],
 });
 
 module.exports = mongoose.model("Student", StudentSchema);
